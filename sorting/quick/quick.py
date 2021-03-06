8384bc426c34f9ab0ca89cac7d41eb0948672688
CosmicCatnap <cosmiccatnap@gmail.com>
2021-03-06 15:38:23 -0500

    working through the interesting problems of attempting to visualizing sorting in javascript when everything is rendered async

--- sorting/quick/quick.py
+++ sorting/quick/quick.py
@@ -8,18 +8,24 @@ def genNumbers(size):
 	numbers = [random.randint(0,9) for x in range(size)]
 	return numbers
 
-def bubbleSort(numbers):
-	n = len(numbers)	
-	count = 0
-	for i in range(n):
-		for j in range(n-i-1):
-			#print(numbers)
-			if numbers[j] > numbers[j+1]:
-				count = count + 1
-				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-	print("sort took " + str(count) + " operations to complete")
+def split(array):
+	if len(array) < 2:
+		return array
+	else:
+		splitArray = []
+		pivot = array[0]
+		for i in array:
+			if i > pivot:
+				splitArray.append(i)
+				print(splitArray)
+		return split(splitArray)
+
+def quickSort(numbers):
+	numbers = split(numbers)
+	#print("sort took " + str(count) + " operations to complete")
 	return numbers
+
 unsortedNumbers = genNumbers(20)
 print("Unsorted: " + str(unsortedNumbers))
-sortedNumbers = bubbleSort(unsortedNumbers)
+sortedNumbers = quickSort(unsortedNumbers)
 print("Sorted: " + str(sortedNumbers))

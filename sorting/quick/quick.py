43730b361ab27281c6eab4cb1eca22454df0acd5
CosmicCatnap <cosmiccatnap@gmail.com>
2021-03-18 23:37:54 -0400

    finally figured out how quicksort AND global python variables work

--- sorting/quick/quick.py
+++ sorting/quick/quick.py
@@ -3,29 +3,45 @@
 
 import random
 
-# Generate random numbers to sort
-def genNumbers(size):
-	numbers = [random.randint(0,9) for x in range(size)]
-	return numbers
+# performance tracking variables
+swaps = 0
+comparisons = 0
 
-def split(array):
-	if len(array) < 2:
+# Generate random array to sort
+def genNumbers(size, scale):
+	array = [random.randint(0,scale) for x in range(size)]
+	return array
+
+def partition(array, start, end):
+	global swaps
+	global comparisons
+	i = (start - 1)
+	pivot = array[end]	
+	for j in range(start, end):
+		comparisons += 1
+		if array[j] <= pivot:
+			i += 1
+			swaps += 1
+			array[i], array[j] = array[j], array[i]
+	swaps += 1
+	array[i+1], array[end] = array[end], array[i+1]
+	return (i+1)
+
+def quickSort(array, start, end):
+	global swaps
+	global comparisons
+	if len(array) == 1:
 		return array
-	else:
-		splitArray = []
-		pivot = array[0]
-		for i in array:
-			if i > pivot:
-				splitArray.append(i)
-				print(splitArray)
-		return split(splitArray)
+	comparisons += 1
+	if start < end:
+		parted = partition(array, start, end)
+		quickSort(array, start, parted - 1)
+		quickSort(array, parted + 1, end)
 
-def quickSort(numbers):
-	numbers = split(numbers)
-	#print("sort took " + str(count) + " operations to complete")
-	return numbers
-
-unsortedNumbers = genNumbers(20)
-print("Unsorted: " + str(unsortedNumbers))
-sortedNumbers = quickSort(unsortedNumbers)
-print("Sorted: " + str(sortedNumbers))
+array = genNumbers(20, 50)
+print("Unsorted: " + str(array))
+quickSort(array, 0, len(array) - 1)
+print("Sorted: " + str(array))
+print("number of items: " + str(len(array)))
+print("comparisons: " + str(comparisons))
+print("swaps: " + str(comparisons))
